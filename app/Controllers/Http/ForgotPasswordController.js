a2767aecc8be16557fa48a3e4ada73e8fcbf9551
Matheus Duarte <matheus.duarte@uaufi.com>
2021-01-07 17:27:19 -0300

    Email de recuperacao de senha

--- app/Controllers/Http/ForgotPasswordController.js
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -2,6 +2,7 @@
 
 const crypto = require('crypto')
 const User = use('App/Models/User')
+const Mail = use('Mail')
 
 class ForgotPasswordController {
   async store ({ request, response }) {
@@ -13,6 +14,20 @@ class ForgotPasswordController {
       user.token_created_at = new Date()
 
       await user.save()
+
+      await Mail.send(
+        ['emails.forgot_password'],
+        {
+          email,
+          token: user.token,
+          link: `${request.input('redirect_url')}?token=${user.token}`
+        },
+        message => {
+          message
+            .from('matheus.duarte@empresa.com', 'Matheus Duarte')
+            .to(email)
+            .subject('Recuperação de senha')
+        })
     } catch (error) {
       return response
         .status(error.status)
